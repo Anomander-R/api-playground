81c73d49d4abd499f512a37f99b59adcdcfe1fc7
Anomander-R <orhan.sabanac@gmail.com>
2022-12-01 16:36:05 +0100

    Twilio testing in NodeJS completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const { Twilio } = require("twilio");
-//const twilio = require('twilio');
+
 
 const { TWILIO_ACCOUNT_SSD, TWILIO_AUTH_TOKEN } = process.env;
 
-//const client = new Twilio(TWILIO_ACCOUNT_SSD, TWILIO_AUTH_TOKEN)
+const client = new Twilio(TWILIO_ACCOUNT_SSD, TWILIO_AUTH_TOKEN)
 
 console.log("Test");
 //console.log(process.env.TWILIO_ACCOUNT_SSD);
@@ -14,3 +14,10 @@ console.log(process.env.TWILIO_BIH_NUMBER);
 console.log("-------------------------------------");
 console.log(TWILIO_AUTH_TOKEN);
 console.log(TWILIO_ACCOUNT_SSD);
+
+client.messages.list()
+    .then(messages => {
+        console.log(`The last message is ${messages[0].body}`)
+    })
+    .catch(error => console.log(error));
+console.log('Gathering your message log!');
