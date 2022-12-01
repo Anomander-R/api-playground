853099019959b08785ffbfbe589bbd1b4559094d
Anomander-R <orhan.sabanac@gmail.com>
2022-12-01 15:38:37 +0100

    Account SSD & Account Auth Token for Twilio added together with dotenv package

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Test');
+require("dotenv").config();
+const { Twilio } = require("twilio");
+//const twilio = require('twilio');
+
+const { TWILIO_ACCOUNT_SSD, TWILIO_AUTH_TOKEN } = process.env;
+
+//const client = new Twilio(TWILIO_ACCOUNT_SSD, TWILIO_AUTH_TOKEN)
+
+console.log("Test");
+//console.log(process.env.TWILIO_ACCOUNT_SSD);
+//console.log(process.env.TWILIO_AUTH_TOKEN);
+console.log(process.env.TWILIO_USA_NUMBER);
+console.log(process.env.TWILIO_BIH_NUMBER);
+console.log("-------------------------------------");
+console.log(TWILIO_AUTH_TOKEN);
+console.log(TWILIO_ACCOUNT_SSD);
